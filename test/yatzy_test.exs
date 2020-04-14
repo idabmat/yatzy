ae36a5e6ad0915b4063a52caa21d755dba611874
Igor de Alcantara Barroso <idabmat@gmail.com>
2020-04-13 22:33:52 -0300

    A high level test

--- test/yatzy_test.exs
+++ test/yatzy_test.exs
@@ -2,7 +2,14 @@ defmodule YatzyTest do
   use ExUnit.Case
   doctest Yatzy
 
-  test "starts the game" do
-    assert Yatzy.new_game() == %{}
+  test "Playing a solo game" do
+    game_state =
+      Yatzy.new_game(["Alice"])
+      |> Yatzy.roll("Alice")
+      |> Yatzy.roll("Alice", fix: [4, 5])
+      |> Yatzy.score("Alice", 1)
+      |> Yatzy.end_game()
+
+    assert game_state[:result] == "Alice won."
   end
 end
