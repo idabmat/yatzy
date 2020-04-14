193861fe8fa484353e4880b0b674f23c17601456
Igor de Alcantara Barroso <idabmat@gmail.com>
2020-04-13 22:39:42 -0300

    Shameless green

--- lib/yatzy.ex
+++ lib/yatzy.ex
@@ -4,15 +4,47 @@ defmodule Yatzy do
   """
 
   @doc """
-  Starting a new game
+  Starting a game
 
   ## Examples
 
-      iex> Yatzy.new_game()
+      iex> Yatzy.new_game(["Alice", "Bob"])
       %{}
 
   """
-  def new_game do
-    %{}
-  end
+  def new_game(_players), do: %{}
+
+  @doc """
+  Rolling the dices for a given player
+
+  ## Examples
+
+      iex> Yatzy.roll(%{}, "Alice")
+      %{}
+
+  """
+  def roll(game_state, player), do: roll(game_state, player, [])
+  def roll(_game_state, _player, _opts), do: %{}
+
+  @doc """
+  Scoring the current roll for a player
+
+  ## Examples
+
+      iex> Yatzy.score(%{}, "Alice", 1)
+      %{}
+
+  """
+  def score(_game_state, _player, _rule), do: %{}
+
+  @doc """
+  Ending a game
+
+  ## Examples
+
+      iex> Yatzy.end_game(%{})
+      %{ result: "Alice won." }
+
+  """
+  def end_game(_game_state), do: %{ result: "Alice won." }
 end
