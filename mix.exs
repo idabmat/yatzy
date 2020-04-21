e47d3e1627206372e5b692166dd6092790c359bc
Igor de Alcantara Barroso <igor@talimhq.com>
2020-04-21 01:47:30 -0300

    Try to spread action jobs (#3)
    * Try to spread action jobs
    
    * Use differenct cache for dialyzer

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule Yatzy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.3", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
